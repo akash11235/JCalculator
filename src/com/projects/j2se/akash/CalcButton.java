300bc9beeaaca14e0d8bf535b761f5e66d8d4701
Akash Pandey <akskipadhai@gmail.com>
2017-06-05 21:39:31 +0530

    initial commit

---
+++ src/com/projects/j2se/akash/CalcButton.java
@@ -0,0 +1,11 @@
+package com.projects.j2se.akash;
+import javax.swing.JButton;
+public class CalcButton extends JButton
+{
+    public CalcButton()
+    {
+      super();
+     }
+ }
+
+
---
+++ src/com/projects/j2se/akash/CalcDisplayArea.java
@@ -0,0 +1,10 @@
+package com.projects.j2se.akash;
+import javax.swing.JTextArea;
+public class CalcDisplayArea extends JTextArea
+{
+    public CalcDisplayArea()
+    {
+      super();
+     }
+ }
+
